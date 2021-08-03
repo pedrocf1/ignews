c94ca9f302b21ef96e558b3442b8e92818029a16
Pedro Freire <pedrofc31.31@gmail.com>
2021-08-03 19:05:39 -0300

    setting global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>dale mundo</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        dale mundo
+      </h1>
+    </>
   )
 }
