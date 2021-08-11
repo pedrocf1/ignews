b5c5e18f3faac3620f1d3dedb7cd1e654b75ef90
Pedro Freire <pedrofc31.31@gmail.com>
2021-08-11 20:07:09 -0300

    payment with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripejs';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,11 +8,34 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+    const [session] = useSession();
+    
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        // criação checkout session
+        try{
+            console.log("VOU FAZER O POST")
+            const response = await api.post('/subscribe')
+            console.log("FIZ O POST")
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+        }catch(error){
+            alert(error.message)
+        }
+    }
 
     return(
         <button
          type="button"
          className={styles.subscribeButton}
+         onClick={handleSubscribe}
         >
             Subscribe now
         </button>
