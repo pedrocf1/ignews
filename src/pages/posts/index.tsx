fa8709beb6232c8b442054ad83203d361444d53d
Pedro Freire <pedrofc31.31@gmail.com>
2021-08-17 19:24:44 -0300

    listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,19 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+    slug: string
+    title: string
+    excerpt: string
+    updatedAt: string
+}
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,11 +24,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a>
-                        <time>12 de março de 2021</time>
-                        <strong>creating lalala</strong>
-                        <p>in the guide lá lá lá lá</p>
-                    </a>
+                    { posts.map(post => (
+                        <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -34,11 +47,24 @@ export const getStaticProps: GetStaticProps = async ()=> {
         pageSize: 100,
     })
 
-    console.log('response', response)
+    console.log('response', JSON.stringify(response, null, 2))
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
         props:{
-
+            posts
         }
     }
 }
