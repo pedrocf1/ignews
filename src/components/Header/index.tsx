29bc9b98a1fad6615c84590e39f6b2751a5ddbbe
Pedro Freire <pedrofc31.31@gmail.com>
2021-08-17 19:56:05 -0300

    creating link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,22 @@
 import { SingInButton } from '../SingInButton';
 import styles from './styles.module.scss'
 import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="logo img" />
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink  activeClassName={styles.active} href='/posts' prefetch>
                         <a>Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <SingInButton/>
